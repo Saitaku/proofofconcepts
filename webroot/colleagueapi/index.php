0bd71b790dc9223bd69e66f9180584169abdef7c
scottmetcalf <scott.metcalf@fresno.edu>
2016-03-26 13:59:15 -0700

    Fix spacing on colleagueapi index

--- webroot/colleagueapi/index.php
+++ webroot/colleagueapi/index.php
@@ -2,20 +2,20 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-  <title>Basic Proof of Concept Bootstrap Template</title>
-  <?php \POC\Renderer::render_css();?>
-  <?php \POC\Renderer::render_header_js();?>
-</head>
-<body>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Basic Proof of Concept Bootstrap Template</title>
+    <?php \POC\Renderer::render_css();?>
+    <?php \POC\Renderer::render_header_js();?>
+  </head>
+  <body>
 
-<h1>Hello, world!</h1>
-<?php echo $user->getUsername();?>
+  <h1>Hello, world!</h1>
+  <?php echo $user->getUsername();?>
 
-<?php \POC\Renderer::render_footer_js();?>
-</body>
+  <?php \POC\Renderer::render_footer_js();?>
+  </body>
 </html>
